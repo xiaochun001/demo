84f81626d6f9adee0e0246165a3836a06beff465
liujianhua <1769908045@qq.com>
2020-03-04 12:14:18 +0800

    init

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+    components: {}
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  a {
+    outline: none;
+    text-decoration: none;
+  }
+  #app {
+    background-color: #eef0f3;
+    height: 100vh;
+  }
+</style>
