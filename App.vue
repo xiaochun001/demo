8739a5aff6a2e00a5b8feb9e06110821ce2fff42
dypmo521 <43594856+dypmo521@users.noreply.github.com>
2020-03-19 16:46:32 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -2,29 +2,4 @@
   <div id="app">
     <router-view></router-view>
   </div>
-</template>
 
-<script>
-
-  export default {
-    name: 'App',
-    components: {123456}
-  }
-</script>
-
-<style>
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 01111111;
-  }
-  a {
-    outline: none;
-   
-  }
-  #app {
-    background-color: #eef0f3;
-    height: 100vh;
-  }
-</style>
