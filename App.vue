339703db210131105ff876028b877861b5916157
dypmo521 <43594856+dypmo521@users.noreply.github.com>
2020-03-19 16:48:13 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -6,25 +6,3 @@
 
 <script>
 
-  export default {
-    name: 'App',
-    components: {}
-  }
-</script>
-
-<style>
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 0;
-  }
-  a {
-    outline: none;
-    text-decoration: none;
-  }
-  #app {
-    background-color: #eef0f3;
-    height: 100vh;
-  }
-</style>
